074913739fa10414b359d211e66e14b9e02fac2e
Riham274 <riham.ararawi274@gmail.com>
2025-09-09 02:00:53 +0300

    Refactor UI with Bootstrap

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 $(document).ready(function () {
-    const initialItems = ['JavaScript', 'HTML', 'CSS', 'React', 'Angular', 'Zustand', 'NextJS', 'TypeScript'];
+  const initialItems = ['JavaScript', 'HTML', 'CSS', 'React', 'Angular', 'Zustand', 'NextJS', 'TypeScript'];
 
-    function addItem(text) {
-        const li = $('<li></li>');
-        const span = $('<span></span>').text(text);
-        const editBtn = $('<button>Edit</button>').addClass('edit');
-        const deleteBtn = $('<button>Delete</button>').addClass('delete');
+  function addItem(text) {
+    const li = $('<li></li>').addClass('list-group-item d-flex justify-content-between align-items-center');
+    const span = $('<span></span>').text(text);
 
-        li.append(span, editBtn, deleteBtn);
-        $('#todo-list').append(li);
+    const btnGroup = $('<div></div>').addClass('d-flex gap-2');
+    const editBtn = $('<button>Edit</button>').addClass('btn btn-primary edit');
+    const deleteBtn = $('<button>Delete</button>').addClass('btn btn-danger delete');
 
+    btnGroup.append(editBtn, deleteBtn);
+    li.append(span, btnGroup);
+    $('#todo-list').append(li);
+  }
+
+  for(let i =0 ; i< initialItems.length; i++){
+     addItem(initialItems[i]) ;
+  }
+
+  $('#add-form').submit(function (e) {
+    e.preventDefault();
+    const newItem = $('#new-item').val().trim();
+    if (newItem) {
+      addItem(newItem);
     }
+  });
 
-    for(let i =0 ; i< initialItems.length; i++){
-        addItem(initialItems[i])
-    }
 
-    //submit
-    $('#add-form').submit(function (e){
-        e.preventDefault();
-        const newItem = $('#new-item').val();
-        if (newItem) {
-            addItem(newItem);
-        }
-    });
-
-   //delete 
-   $('#todo-list').on('click', '.delete', function () {
+  $('#todo-list').on('click', '.delete', function () {
     $(this).closest('li').remove();
-    });
+  });
 
-    //edit
-    $('#todo-list').on('click', '.edit', function () {
-      const $btn = $(this);
-      const $li = $btn.parent();
-    
-      if ($btn.text() === 'Edit') {
-         const currentText = $li.find('span').text();
-         $li.find('span').replaceWith(`<input type="text" value="${currentText}">`);
-         $btn.text('Save');
-        } 
-       
-      else {
-        const newText = $li.find('input').val();
-        if (newText) {
-            $li.find('input').replaceWith(`<span>${newText}</span>`);
-            $btn.text('Edit');
-            }
-        }
-    });
- });
+
+  $('#todo-list').on('click', '.edit', function () {
+    const $btn = $(this);
+    const $li = $btn.closest('li');
+
+    if ($btn.text() === 'Edit') {
+      const currentText = $li.find('span').text();
+      $li.find('span').replaceWith(`<input type="text" class="form-control m-1" value="${currentText}">`);
+      $btn.text('Save');
+    } 
+    else {
+      const newText = $li.find('input').val();
+      if (newText) {
+        $li.find('input').replaceWith(`<span>${newText}</span>`);
+        $btn.text('Edit');
+      }
+    }
+  });
+});
